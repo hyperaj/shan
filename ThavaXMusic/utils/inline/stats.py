2e62571f64c072d7e0f98540a791a72d64f84c4d
hyperaj <168776874+hyperaj@users.noreply.github.com>
2024-05-27 11:16:12 +0530

    Update stats.py

--- ThavaXMusic/utils/inline/stats.py
+++ ThavaXMusic/utils/inline/stats.py
@@ -5,7 +5,7 @@ def stats_buttons(_, status):
     not_sudo = [
         InlineKeyboardButton(
             text=_["SA_B_1"],
-            callback_data="𝄞 TopOverall 𝄞",
+            callback_data="🍁 TopOverall 🍂",
         )
     ]
     sudo = [
@@ -15,7 +15,7 @@ def stats_buttons(_, status):
         ),
         InlineKeyboardButton(
             text=_["SA_B_3"],
-            callback_data="𝄞 TopOverall 𝄞",
+            callback_data="🍁 TopOverall 🍂",
         ),
     ]
     upl = InlineKeyboardMarkup(
@@ -24,7 +24,7 @@ def stats_buttons(_, status):
             [
                 InlineKeyboardButton(
                     text=_["CLOSE_BUTTON"],
-                    callback_data="𝄞 close 𝄞",
+                    callback_data="🍁 close 🍂",
                 ),
             ],
         ]
@@ -42,7 +42,7 @@ def back_stats_buttons(_):
                 ),
                 InlineKeyboardButton(
                     text=_["CLOSE_BUTTON"],
-                    callback_data="𝄞 close 𝄞",
+                    callback_data="🍁 close 🍂",
                 ),
             ],
         ]
